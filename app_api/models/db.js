14811372c98b338c02031d2ec7d467515f5da485
alexandru <alexandru_visan@telekom.ro>
2017-07-25 13:31:22 +0300

    Upload sources on GitHub

---
+++ app_api/models/db.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var dbURI = 'mongodb://localhost/Loc8r';
+var gracefulShutDown;
+
+if(process.env.NODE_ENV === 'production'){
+	dbURI = process.env.MONGOLAB_URI;
+}
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function() {
+	console.log('Mongooose connecetd to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+	console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+	console.log('Mongoose disconnected');
+});
+
+var gracefulShutDown = function(msg, callback) {
+	mongoose.connection.close(function() {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+	})
+}
+
+process.once('SIGUSR2', function() {
+	gracefulShutDown('nodemon restart', function() {
+		process.kill(process.pid, 'SIGUSR2');
+	})
+}) ;
+
+process.on('SIGINT', function() {
+	gracefulShutDown('app termination', function() {
+		process.exit(0);
+	})
+});
+
+require('./locations');
+require('./users');
